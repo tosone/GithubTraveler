a5b1d3d85cd7295e362e42cd6c5516068d006631
Tosone <i@tosiney.com>
2018-04-14 21:42:07 +0800

    :memo: Add docs' pictures.
    Signed-off-by: Tosone <i@tosiney.com>

--- main.go
+++ main.go
@@ -20,8 +20,6 @@ var BuildStamp = "no provided"
 var GitHash = "no provided"
 
 func main() {
-	var schol = "release"
-	logging.Info(schol)
 	version.Setting(Version, BuildStamp, GitHash)
 
 	if err := cmd.RootCmd.Execute(); err != nil {
